fd95ff6ec0d10d956886a02320e178345b85ef2c
verinak <verinamichelk@gmail.com>
2025-03-11 05:55:50 +0200

    fft imports

--- DIP_UNET_models/unet_and_tv/data/transforms.py
+++ DIP_UNET_models/unet_and_tv/data/transforms.py
@@ -62,7 +62,7 @@ def fft2(data):
     """
     assert data.size(-1) == 2
     data = ifftshift(data, dim=(-3, -2))
-    data = torch.fft(data, 2, normalized=True)
+    data = torch.fft.fft2(data, norm="ortho")
     data = fftshift(data, dim=(-3, -2))
     return data
 
@@ -81,7 +81,7 @@ def ifft2(data):
     """
     assert data.size(-1) == 2
     data = ifftshift(data, dim=(-3, -2))
-    data = torch.ifft(data, 2, normalized=True)
+    data = torch.fft.ifft2(data, norm="ortho")
     data = fftshift(data, dim=(-3, -2))
     return data
 
